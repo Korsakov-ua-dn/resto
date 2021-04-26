e9fcf6204cb4475c7ff093222f03471161f924da
korsakov <korsakov.ua.dn@gmail.com>
2021-04-26 19:23:27 +0300

    end at 7:30 Lesson 112

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,43 @@
 const initialState = {
     menu: [],
-    loading: true
+    loading: true,
+    error: false,
+    items: [
+        {
+			"title": "Cesar salad",
+			"price": 12,
+			"url": "https://static.1000.menu/img/content/21458/-salat-cezar-s-kr-salat-cezar-s-krevetkami-s-maionezom_1501173720_1_max.jpg",
+			"category": "salads",
+			"id": 1
+		},
+		{
+			"title": "Pizza Margherita",
+			"price": 10,
+			"url": "https://attuale.ru/wp-content/uploads/2018/04/6eaa1be0f1976c5f5a5f2d2b3ae42fc8.jpg",
+			"category": "pizza",
+			"id": 2
+		}
+    ]
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'MENU_LOADED':
             return {
+                ...state,
                 menu: action.payload,
                 loading: false,
                 error: false
             };
         case 'MENU_REQUESTED':
             return {
-                menu: state.menu,
+                ...state,
                 loading: true,
                 error: false
             };
         case 'MENU_ERROR':
             return {
-                menu: state.menu,
+                ...state,
                 loading: false,
                 error: true
             };
